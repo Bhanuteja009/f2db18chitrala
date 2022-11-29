dcd8c5623c28a0d10bbd1cf3f0e78d0e9f67ed8e
Bhanuteja009 <111711705+Bhanuteja009@users.noreply.github.com>
2022-11-29 14:45:38 -0600

    login and logout changes

--- routes/house.js
+++ routes/house.js
@@ -2,6 +2,15 @@ var express = require('express');
 const house_controlers= require('../controllers/house'); 
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on or redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
 /* GET houses */
 router.get('/', house_controlers.house_view_all_Page);
 
@@ -12,7 +21,7 @@ router.get('/detail', house_controlers.house_view_one_Page);
 router.get('/create', house_controlers.house_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', house_controlers.house_update_Page); 
+router.get('/update',secured, house_controlers.house_update_Page); 
 
 /* GET delete costume page */ 
 router.get('/delete', house_controlers.house_delete_Page); 
